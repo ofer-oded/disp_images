11dc015f65b9f60f96c19cd6b10f006856ba3ee8
ofer oded <ofer_oded@yahoo.com>
2019-12-29 19:49:15 +0200

    added aslint hooks removed unused 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/App.js 	modified:   src/components/imageCom.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -5,11 +5,11 @@ import {useState} from 'react';
 
 
 const App = () => {
-    const [dataRequested,setDataRequestedl] = useState(false);
+    //const [dataRequested,setDataRequestedl] = useState(false);
     const [imageCount,setImageCount] = useState(0)
     const handleDisplayControlClick = () =>
     {
-        setDataRequestedl(true);
+      //  setDataRequestedl(true);
         var count = imageCount + 1;
         count =  count === 2 ? 0 : count 
         setImageCount(count);
