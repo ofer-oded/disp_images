505413839f9aaca277e186fa59a7e1b2d1b59d44
ofer oded <ofer_oded@yahoo.com>
2020-05-02 07:36:29 +0300

    Added full screen feature 	modified:   src/components/mainView.js

--- disp_images_frontend/src/components/mainView.js
+++ disp_images_frontend/src/components/mainView.js
@@ -1,8 +1,4 @@
 import React, { useEffect } from 'react'
-//import {useState} from 'react'
-
-//import useResourceToGetURL from '../resources/makeAjaxRequest';
-//import base_url from '../configs/base_url'
 import './mainView.scss'
 
 /*
@@ -11,40 +7,31 @@ responsible for displaying the images
 
 
 const MainView= (props) => {
-    //const nextImageUrl = useResourceToGetURL(imageCount); // request next image
-    //const nextImageUrl = props.imageToBeDisplayed
-    //const fullUrl = `http://${base_url}:8000/media/${nextImageUrl}`;
     const notFoundURL = 'clear.gif';
-    //const [imageLoaded,setImageLoaded] = useState(false)
-    
     var image = document.images[0];
     
+    const handleImageClickEvent = () => {
+        if(image.requestFullscreen){
+            image.requestFullscreen();
+        } else if(image.mozRequestFullScreen){ // Firefox
+            image.mozRequestFullScreen();
+        } else if(image.webkitRequestFullscreen){ // Chrome, Safari and Opera 
+            image.webkitRequestFullscreen();
+        } else if(image.msRequestFullscreen) { // IE/Edge
+            image.msRequestFullscreen();
+       }
+    }
+
     useEffect(()=>{
         if(props.downloadedImage !== null  && props.downloadedImage !== undefined && props.downloadedImage !== '') {
             image.src = props.downloadedImage;
         }
-     
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[props.downloadedImage])
-    //var downloadingImage = new Image();
-    
-    /*
-    downloadingImage.onload = () => {
-        image.src = downloadingImage.src;
-        setImageLoaded(true)
-    }*/
-    
-    //downloadingImage.src = fullUrl;
-
-    /*
-    if(imageLoaded){
-       props.onImageLoad()
-    }*/
-
 
     return(
         <div> 
-        <img src={notFoundURL} alt = "not found" ></img>
+        <img src={notFoundURL} alt = "not found" onClick={handleImageClickEvent} ></img>
         </div>
     );
 }
