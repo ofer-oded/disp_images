82af13cf8e77df7bc68cea84b56f2cd24cab17fb
Ofer Oded <ooded@apple.com>
2021-03-28 21:00:54 +0300

    Server ip changed

--- 2_deploy.sh
+++ 2_deploy.sh
@@ -1,7 +1,7 @@
 # copy docker-compose.yml to remote host
 # destination directory should exist
 echo 'copy docker-compose yml and .env to remote host ...'
-rsync ./docker/.env  itay@192.168.1.116:/home/itay/docker-compose
+rsync ./docker/.env  itay@192.168.1.110:/home/itay/docker-compose
 if [ $? == 0 ]
 then
     echo done
@@ -11,7 +11,7 @@ else
 fi
 
 echo "copy docker-compose yml to remote host"
-rsync ./docker/docker-compose_no_build.yml itay@192.168.1.116:/home/itay/docker-compose/docker-compose.yml
+rsync ./docker/docker-compose_no_build.yml itay@192.168.1.110:/home/itay/docker-compose/docker-compose.yml
 if [ $? == 0 ]
 then
     echo done
@@ -21,7 +21,7 @@ else
 fi
 
 echo "copy deploy script to remote host"
-rsync ./deploy_on_remote_host.sh itay@192.168.1.116:/home/itay
+rsync ./deploy_on_remote_host.sh itay@192.168.1.110:/home/itay
 if [ $? == 0 ]
 then
     echo done
@@ -32,7 +32,7 @@ fi
 
 # connecting to remote host
 echo 'Running deploy script on remote host remote host'
-ssh itay@192.168.1.116 'chmod a+x deploy_on_remote_host.sh'
+ssh itay@192.168.1.110 'chmod a+x deploy_on_remote_host.sh'
 if [ $? == 0 ]
 then
     echo done
@@ -40,7 +40,7 @@ else
     echo failed
     exit 1
 fi
-ssh itay@192.168.1.116 './deploy_on_remote_host.sh'
+ssh itay@192.168.1.110 './deploy_on_remote_host.sh'
 if [ $? == 0 ]
 then
     echo done
--- disp-images-angular-frontend/src/environments/environment.prod.ts
+++ disp-images-angular-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "192.168.1.116"
+  baseUrl: "192.168.1.110"
 };
