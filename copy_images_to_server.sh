f4275b3e3376762200f89478e539232a6fec88b8
Ofer Oded <ooded@apple.com>
2021-04-10 09:10:39 +0300

    Tag 3.0

--- copy_images_to_server.sh
+++ copy_images_to_server.sh
@@ -3,4 +3,4 @@ src_dir=/Users/oferoded/Documents/MySlides/
 dst_remote_dir=/home/itay/temp/
 
 echo copy images from $src_dir to $dst_remote_dir
-rsync -v $src_dir*.jpg itay@192.168.1.116:$dst_remote_dirdocker 
+rsync -v $src_dir*.jpg itay@192.168.1.110:$dst_remote_dirdocker 
