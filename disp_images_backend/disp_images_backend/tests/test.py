057ad5a40137a9592d7d102354aab9beac863c43
Ofer Oded <ooded@apple.com>
2021-05-28 16:18:01 +0300

    completed backend test

--- disp_images_backend/disp_images_backend/tests/test.py
+++ disp_images_backend/disp_images_backend/tests/test.py
@@ -26,52 +26,50 @@ class TestCalls(TestCase):
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
         self.assertEqual(eval(response.content)['image_path'],'1999__eventB/20150903_224212.jpg')
 
-        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_PREV_IMAGE_DETAILS})
+        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_NEXT_IMAGE_DETAILS})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(eval(response.content)['image_index'],1)
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'1999__eventB/20150904_044359.jpg')
+        self.assertEqual(eval(response.content)['image_path'],'1999__eventB/20150904_044359.jpg')
 
-
-
-        response = self.client.get('/disp_images/',{'command':'GET_NEXT_IMAGE_NAME'})
+        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_NEXT_IMAGE_DETAILS})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(eval(response.content)['image_index'],2)
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'1999__eventB/20150904_044359.jpg')
+        self.assertEqual(eval(response.content)['image_path'],'1999__eventB/20150904_050923.jpg')
 
 
-        response = self.client.get('/disp_images/',{'command':'GET_NEXT_IMAGE_NAME'})
+        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_NEXT_IMAGE_DETAILS})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(eval(response.content)['image_index'],3)
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'2001__eventA/20150904_040242.jpg')
+        self.assertEqual(eval(response.content)['image_path'],'2001__eventA/20150904_040242.jpg')
 
 
-        response = self.client.get('/disp_images/',{'command':'GET_NEXT_IMAGE_NAME'})
+        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_NEXT_IMAGE_DETAILS})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(eval(response.content)['image_index'],4)
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'2001__eventA/20150905_054750.jpg')
+        self.assertEqual(eval(response.content)['image_path'],'2001__eventA/20150905_054750.jpg')
 
-        response = self.client.get('/disp_images/',{'command':'GET_NEXT_IMAGE_NAME'})
+
+        response = self.client.get('/disp_images/', {'command': RequestCommands.GET_NEXT_IMAGE_DETAILS})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(eval(response.content)['image_index'],0)
-        self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'1999__eventB/20150903_224212.jpg')
+        self.assertEqual(eval(response.content)['image_index'], 0)
+        self.assertEqual(eval(response.content)['total_number_of_images'], 5)
+        self.assertEqual(eval(response.content)['image_path'], '1999__eventB/20150903_224212.jpg')
 
+        response = self.client.get('/disp_images/',{'command':RequestCommands.GET_NEXT_IMAGE_DETAILS})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(eval(response.content)['image_index'],1)
+        self.assertEqual(eval(response.content)['total_number_of_images'],5)
+        self.assertEqual(eval(response.content)['image_path'],'1999__eventB/20150904_044359.jpg')
+
+
+        response = self.client.get('/disp_images/',{'command':RequestCommands.READ_IMAGE_DETAILS})
+        self.assertEqual(response.status_code, 200)
 
 
         response = self.client.get('/disp_images/',{'command':'RESTART'})
         self.assertEqual(response.status_code, 200)
-
-        response = self.client.get('/disp_images/',{'command':'GET_NEXT_IMAGE_NAME'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(eval(response.content)['image_index'],0)
         self.assertEqual(eval(response.content)['total_number_of_images'],5)
-        self.assertEqual(eval(response.content)['image_name'],'1999__eventB/20150903_224212.jpg')
-
-
-        response = self.client.get('/disp_images/',{'command':'NOT_SUPPORTED_COMMAND'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(eval(response.content)['image_index'],-1)
